6c1db9eef84d81ebabbc9c74ef4b39aa2aa5130a
Anas AbuGhalieh <civil.Anas1000@gmail.com>
2021-08-31 15:25:54 +0300

    finish the lab

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
 # Create your models here.
+from django.urls import reverse
+
+class Snack(models.Model):
+    title= models.CharField(max_length=64)
+    purchaser= models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    description=models.TextField()
+    
+    def __str__ (self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('snack_detail', args=[self.id])
--- snacks_crud_project/settings.py
+++ snacks_crud_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "snacks"
+    
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'snacks_crud_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
